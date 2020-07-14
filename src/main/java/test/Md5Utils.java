01243640f5d57d116af9dd9a9eb5e45b7fd1f2b6
swu <swu@joinhealth.cn>
2020-07-14 11:08:07 +0800

    !!

---
+++ src/main/java/test/Md5Utils.java
@@ -0,0 +1,58 @@
+package test;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Objects;
+
+/**
+ * Md5Utils
+ *
+ * @author swu
+ * @date 2020-03-03
+ */
+public class Md5Utils {
+
+    private static byte[] md5(String s) {
+        MessageDigest algorithm;
+        try {
+            algorithm = MessageDigest.getInstance("MD5");
+            algorithm.reset();
+            algorithm.update(s.getBytes(StandardCharsets.UTF_8));
+            return algorithm.digest();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String toHex(byte[] hash) {
+        if (hash == null) {
+            return null;
+        }
+        StringBuilder buf = new StringBuilder(hash.length * 2);
+        int i;
+
+        for (i = 0; i < hash.length; i++) {
+            if ((hash[i] & 0xff) < 0x10) {
+                buf.append("0");
+            }
+            buf.append(Long.toString(hash[i] & 0xff, 16));
+        }
+        return buf.toString();
+    }
+
+    /**
+     * 将string进行md5编码
+     * @param s
+     * @return
+     */
+    public static String encode(String s) {
+        try {
+            return new String(Objects.requireNonNull(toHex(md5(s))).getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return s;
+        }
+    }
+}
+
